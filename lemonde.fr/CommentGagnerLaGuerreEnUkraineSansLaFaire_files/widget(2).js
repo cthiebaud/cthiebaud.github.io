4fe95adfab69a3a3aa5c8592ec3d22318c6048d5
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2022-03-16 10:15:14 +0100

    CommentGagnerLaGuerreEnUkraineSansLaFaire

---
+++ lemonde.fr/CommentGagnerLaGuerreEnUkraineSansLaFaire_files/widget(2).js
@@ -0,0 +1,53 @@
+function heredoc(f) {return f.toString().match(/\/\*\s*([\s\S]*?)\s*\*\//m)[1];};var fw_most_read_recommendations = heredoc(function(){/*  <style>
+            .fw_most_read_recommendations {
+                background-color: #f5f6f8;
+                border-top: 4px solid #2a303b;
+                padding: 0 20px 20px 20px;
+            }
+            .fw_most_read_recommendations-articles-type {
+                font-family: "Marr Sans Condensed",DINCondensed-Bold,"Roboto Condensed","Arial Narrow",sans-serif;
+                font-size: 28px;
+                line-height: 1.4;
+                font-weight: 700;
+                margin: 0;
+                margin-top: 28px;
+                color: #2a303b;
+            }
+            .fw_most_read_recommendations-articles {
+                margin: 0;
+                padding: 0;
+                background-color: hsl(220, 18%, 97%);
+                list-style: none;
+            }
+            .fw_most_read_recommendations-article:not(:last-child) {
+                border-bottom: 1px solid #d5d8dc;
+            }
+            .fw_most_read_recommendations-article a {
+                display: flex;
+                padding: 24px 0;
+                text-decoration: none;
+                color: #2a303b;
+            }
+            .fw_most_read_recommendations-article-number {
+                font-family: "The Antiqua B",Georgia,Droid-serif,serif;
+                margin-top: -3px;
+                display: flex;
+                font-weight: 600;
+                line-height: normal;
+                color: #2a303b;
+                font-size: 32px;
+            }
+            .fw_most_read_recommendations-article-title  {
+                font-family: "The Antiqua B",Georgia,Droid-serif,serif;
+                display: flex;
+                color: #2a303b;
+                font-size: 18px;
+                line-height: 1.3;
+                letter-spacing: -0.02px;
+                font-weight: 400;
+                padding-left: 16px;
+            }
+            .fw_most_read_recommendations-article a:hover .fw_most_read_recommendations-article-title {
+                text-decoration: underline;
+            }
+        </style>   <div class="fw_most_read_recommendations"> <p class="fw_most_read_recommendations-articles-type">  Les plus lus  </p> <ul class="fw_most_read_recommendations-articles">  <li class="fw_most_read_recommendations-article"> <a href="https://www.lemonde.fr/international/live/2022/03/16/guerre-en-ukraine-kiev-sous-couvre-feu-zelensky-va-s-exprimer-devant-le-congres-americain_6117694_3210.html"> <span class="fw_most_read_recommendations-article-number">1</span>  <span class="fw_most_read_recommendations-article-title">La guerre en Ukraine, en direct : plusieurs explosions retentissent à Kiev, sous couvre-feu</span>  </a> </li>  <li class="fw_most_read_recommendations-article"> <a href="https://www.lemonde.fr/international/live/2022/03/15/guerre-en-ukraine-en-direct-la-pologne-demande-a-l-otan-de-mettre-en-place-une-mission-de-paix-en-ukraine-l-electricite-est-retablie-a-tchernobyl_6117546_3210.html"> <span class="fw_most_read_recommendations-article-number">2</span>  <span class="fw_most_read_recommendations-article-title">Guerre en Ukraine, en direct : la Pologne demande à l’OTAN de mettre en place une « mission de paix » en Ukraine, l’électricité est rétablie à Tchernobyl</span>  </a> </li>  <li class="fw_most_read_recommendations-article"> <a href="https://www.lemonde.fr/international/article/2022/03/16/guerre-en-ukraine-deux-journalistes-d-une-equipe-de-fox-news-tues-pres-de-kiev_6117671_3210.html"> <span class="fw_most_read_recommendations-article-number">3</span>  <span class="fw_most_read_recommendations-article-title">Guerre en Ukraine : deux journalistes d’une équipe de Fox News tués près de Kiev</span>  </a> </li>  </ul>  </div>  */}); 
