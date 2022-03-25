b944b23ffc10fd3616807a04a565498a2a53f692
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2022-03-25 08:30:44 +0100

    cyberukraine

---
+++ lemonde.fr/cyberukraine_files/widget(2).js
@@ -0,0 +1,53 @@
+function heredoc(f) {return f.toString().match(/\/\*\s*([\s\S]*?)\s*\*\//m)[1];};var fw_most_read_recommendations = heredoc(function(){/*  <style>
+            .fw_most_read_recommendations {
+                background-color: #f5f6f8;
+                border-top: 4px solid #2a303b;
+                padding: 0 20px 20px 20px;
+            }
+            .fw_most_read_recommendations-articles-type {
+                font-family: "Marr Sans Condensed",DINCondensed-Bold,"Roboto Condensed","Arial Narrow",sans-serif;
+                font-size: 28px;
+                line-height: 1.4;
+                font-weight: 700;
+                margin: 0;
+                margin-top: 28px;
+                color: #2a303b;
+            }
+            .fw_most_read_recommendations-articles {
+                margin: 0;
+                padding: 0;
+                background-color: hsl(220, 18%, 97%);
+                list-style: none;
+            }
+            .fw_most_read_recommendations-article:not(:last-child) {
+                border-bottom: 1px solid #d5d8dc;
+            }
+            .fw_most_read_recommendations-article a {
+                display: flex;
+                padding: 24px 0;
+                text-decoration: none;
+                color: #2a303b;
+            }
+            .fw_most_read_recommendations-article-number {
+                font-family: "The Antiqua B",Georgia,Droid-serif,serif;
+                margin-top: -3px;
+                display: flex;
+                font-weight: 600;
+                line-height: normal;
+                color: #2a303b;
+                font-size: 32px;
+            }
+            .fw_most_read_recommendations-article-title  {
+                font-family: "The Antiqua B",Georgia,Droid-serif,serif;
+                display: flex;
+                color: #2a303b;
+                font-size: 18px;
+                line-height: 1.3;
+                letter-spacing: -0.02px;
+                font-weight: 400;
+                padding-left: 16px;
+            }
+            .fw_most_read_recommendations-article a:hover .fw_most_read_recommendations-article-title {
+                text-decoration: underline;
+            }
+        </style>   <div class="fw_most_read_recommendations"> <p class="fw_most_read_recommendations-articles-type">  Les plus lus  </p> <ul class="fw_most_read_recommendations-articles">  <li class="fw_most_read_recommendations-article"> <a href="https://www.lemonde.fr/international/live/2022/03/24/guerre-en-ukraine-en-direct-joe-biden-evoque-de-nouvelles-sanctions-contre-la-russie-et-promet-une-reponse-de-l-otan-en-cas-de-recours-a-des-armes-chimiques_6118889_3210.html"> <span class="fw_most_read_recommendations-article-number">1</span>  <span class="fw_most_read_recommendations-article-title">Guerre en Ukraine en direct : Joe Biden évoque de nouvelles sanctions contre la Russie et promet une « réponse » de l’OTAN en cas de recours à des armes chimiques</span>  </a> </li>  <li class="fw_most_read_recommendations-article"> <a href="https://www.lemonde.fr/international/live/2022/03/25/guerre-en-ukraine-en-direct-a-marioupol-des-habitants-commencent-a-mourrir-de-faim-a-bruxelles-les-occidentaux-peinent-a-tracer-une-ligne-rouge_6119057_3210.html"> <span class="fw_most_read_recommendations-article-number">2</span>  <span class="fw_most_read_recommendations-article-title">Guerre en Ukraine en direct : à Marioupol, des habitants « commencent à mourir de faim », les Occidentaux peinent à tracer une ligne rouge à Bruxelles</span>  </a> </li>  <li class="fw_most_read_recommendations-article"> <a href="https://www.lemonde.fr/international/article/2022/03/25/a-bruxelles-les-occidentaux-unis-mais-demunis-face-a-la-guerre-en-ukraine_6119055_3210.html"> <span class="fw_most_read_recommendations-article-number">3</span>  <span class="fw_most_read_recommendations-article-title">A Bruxelles, les Occidentaux unis mais démunis face à la guerre en Ukraine</span>  </a> </li>  </ul>  </div>  */}); 
